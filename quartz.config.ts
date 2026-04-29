27f282cb9ba3e194c7ac8137c0557944c65acf35
LaurenceHill <laurencehill13@gmail.com>
2026-04-29 09:57:31 +0000

    Remove custom domain, use default github.io URL

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "coeus.llmwiki.com",
+    baseUrl: "laurencehill.github.io/Coeus_Wiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
